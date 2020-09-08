78ce3e207f0911f17a054ee89d2293385743cd4e
Nathan Steed <steedn@outlook.com>
2020-09-08 19:16:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
-
-  create_table "songs", force: :cascade do |t|
-    t.string "name"
-    t.integer "artist_id"
-    t.integer "genre_id"
-  end
+ActiveRecord::Schema.define(version: 2) do
 
 end
